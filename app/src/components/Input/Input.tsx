2fbe2dc91165363181ff3caef00ccb17b7ecd111
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-26 20:13:16 +0545

    feat: added input component

---
+++ app/src/components/Input/Input.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./Input.css";
+import { EyeIcon, EyeSlashIcon } from "components/Icons/EyeIcons";
+
+interface InputProps {
+  name: string;
+  id: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  show?: boolean;
+  type: "text" | "password" | "email";
+}
+
+export function Input({
+  name,
+  id,
+  value,
+  onChange = () => {},
+  placeholder = "",
+}: InputProps) {
+  return (
+    <input
+      id={id}
+      className="input"
+      type="text"
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
+}
+
+export function InputPassword({
+  name,
+  id,
+  value,
+  show = false,
+  onChange = () => {},
+  placeholder = "",
+}: InputProps) {
+    const [showPassword, setShowPassword] = useState(show);
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
+
+  return (
+    <>
+      <div className="input-password">
+        <input
+          id={id}
+          className="input"
+          type={showPassword ? "text" : "password"}
+          name={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+        {
+        showPassword ? (
+            <EyeIcon onClick={toggleShowPassword} className="eye-svg"/>
+          ) : (
+            <EyeSlashIcon onClick={toggleShowPassword} className="eye-svg"/>
+        )}
+
+      </div>
+    </>
+  );
+}
