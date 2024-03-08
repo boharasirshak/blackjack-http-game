219166e8eda7742230315774229dbdedd3831133
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-08 23:45:39 +0545

    feat: private route error page

---
+++ app/src/pages/PrivateRoute.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navigate } from 'react-router-dom';
+
+interface PrivateRouteProps {
+  component: React.FC;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ component: Component, ...rest }) => {
+  const isAuthenticated = localStorage.getItem('token') !== null;
+  return (
+    <>
+      {isAuthenticated ? (
+      <>
+        <Component />
+        {rest} 
+      </>
+      ) : <Navigate to="/" replace />}
+    </>
+  );
+};
+
+export default PrivateRoute;
