ef6829a39935c44009a25f640cf0ae5feaee5b11
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-08 23:45:21 +0545

    feat: added routes

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ErrorPage from "./pages/ErrorPage";
+
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/error",
+    element: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+const node = document.getElementById("root") as HTMLElement;
+
+createRoot(node).render(
+  <RouterProvider router={router} />
+);
