f00d93c4c03fcbf35a7a7aeef397cfe80c82eb02
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-08 23:44:20 +0545

    feat: the landing page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Button from "./components/Button";
+import { useLocalStorageString } from "./hooks/useLocalStorage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  document.title = "Блэкджек";
+
+  const [token] = useLocalStorageString("token");
+  if (token) {
+    window.location.href = "/dashboard";
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className="container">
+        <h1>Блэкджек</h1>
+        <a href="/login">
+          <Button>Авторизоваться</Button>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <span>или</span>
+        <a href="/signup">
+          <Button>Регистрация</Button>
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
