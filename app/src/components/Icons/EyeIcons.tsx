ccc184f4ebe06c0ef913e03a9e16979cdc017e02
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-08 23:44:11 +0545

    feat: added arrows and icons

---
+++ app/src/components/Icons/EyeIcons.tsx
@@ -0,0 +1,51 @@
+export const EyeIcon = ({
+  onClick,
+  className,
+}: {
+  onClick: () => void;
+  className: string;
+}) => (
+  <svg
+    className={className}
+    onClick={onClick}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+    <circle cx="12" cy="12" r="3" />
+  </svg>
+);
+
+// SVG for the eye-slash icon
+export const EyeSlashIcon = ({
+  onClick,
+  className,
+}: {
+  onClick: () => void;
+  className: string;
+}) => (
+  <svg
+    className={className}
+    onClick={onClick}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M17.5 17.5L6.5 6.5" />
+    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+    <circle cx="12" cy="12" r="3" />
+  </svg>
+);
