50f077202e6156d6f3dd2ac19573dcce1e6e5dd5
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-08 22:22:15 +0545

    feat: added ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,16 @@
 module.exports = {
     apps: [
       {
-        name: "backend",
+        name: "backend-blackjack",
         cwd: "./server",
         script: "npm",
         args: "start",
-      }
+      },
+      // {
+      //   name: "backend-frontend",
+      //   cwd: "./app",
+      //   script: "npm",
+      //   args: "start",
+      // }
     ],
 };
