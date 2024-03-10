f4cf53db569f0744a85aa47be4ece1788c5db615
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-10 22:40:53 +0545

    feat: added /game/:code route

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,8 +4,10 @@ import './index.css';
 
 import Dashboard from "./pages/Dashboard";
 import ErrorPage from "./pages/ErrorPage";
+import Game from "./pages/Game";
 import Games from "./pages/Games";
 import Login from "./pages/Login";
+import PrivateRoute from "./pages/PrivateRoute";
 import Signup from "./pages/Signup";
 
 import App from "./App";
@@ -32,12 +34,17 @@ const router = createBrowserRouter([
   },
   {
     path: "/dashboard",
-    element: <Dashboard />,
+    element: <PrivateRoute component={Dashboard} />,
     errorElement: <ErrorPage />,
   },
   {
     path: "/games",
-    element: <Games />,
+    element: <PrivateRoute component={Games} />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/game/:code",
+    element: <PrivateRoute component={Game} />,
     errorElement: <ErrorPage />,
   },
 ]);
