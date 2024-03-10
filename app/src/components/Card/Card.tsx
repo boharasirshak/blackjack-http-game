cafc844f79ebb5309dccfe387a6d9906f71a203c
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-10 22:41:07 +0545

    feat: added card component

---
+++ app/src/components/Card/Card.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import "./Card.css";
+
+type CardProps = {
+  value: string;
+  suit: string;
+  hidden: boolean;
+};
+
+const Card: React.FC<CardProps> = ({ value, suit, hidden }) => {
+  const getColor = () => {
+    if (suit === '♠' || suit === '♣') {
+      return 'black';
+    }
+    else {
+      return'red';
+    }
+  }
+
+  const getCard = () => {
+    if (hidden) {
+      return (
+        <div className={`hiddenCard`} />
+      );
+    }
+    else {
+      return (
+        <div className={`card`}>
+          <div className={getColor()}>
+            <h1 className={`value`}>{value}</h1>
+            <h1 className={`suit`}>{suit}</h1>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <>
+    {getCard()}
+    </>
+  );
+}
+
+export default Card
