247a181ae376cc201a52e3fe0331dd599c9f8302
Sirshak Bohara <boharasirshak101@gmail.com>
2024-03-11 21:58:54 +0545

    feat: added getTotalScore procedure to calculate total of cards

--- server/sql/cards.sql
+++ server/sql/cards.sql
@@ -80,3 +80,45 @@ COMMENT 'Procreudre to get a single card from the cards table using cardId
 BEGIN
     SELECT * FROM cards WHERE id = i_cardId;
 END;
+
+DROP PROCEDURE IF EXISTS getTotalScore;
+CREATE PROCEDURE getTotalScore(player_id INT, OUT total_score INT)
+COMMENT 'Procedure to calculate the total score of a player in a game
+         player_id INT - the id of the player
+         total_score INT - the total score of the player'
+BEGIN
+    DECLARE ace_count INT DEFAULT 0;
+    DECLARE other_cards_total INT DEFAULT 0;
+    DECLARE total_score INT DEFAULT 0;
+    
+    -- Count the number of Aces in the player's hand
+    SELECT COUNT(*) INTO ace_count
+    FROM player_hands
+    JOIN cards ON player_hands.card_id = cards.id
+    WHERE player_hands.player_id = player_id AND cards.value = 'A';
+    
+    -- Calculate the total value of non-Ace cards
+    SELECT SUM(CASE 
+                 WHEN cards.value IN ('J', 'Q', 'K') THEN 10
+                 ELSE IF(cards.value = 'A', 0, CAST(cards.value AS UNSIGNED)) -- Assuming numeric values for cards
+               END) INTO other_cards_total
+    FROM player_hands
+    JOIN cards ON player_hands.card_id = cards.id
+    WHERE player_hands.player_id = player_id AND cards.value <> 'A';
+    
+    -- Initialize total score with the total of non-Ace cards
+    SET total_score = other_cards_total;
+    
+    -- Adjust the score for each Ace in the hand
+    WHILE ace_count > 0 DO
+        IF (total_score + 11) > 21 THEN
+            SET total_score = total_score + 1;
+        ELSE
+            SET total_score = total_score + 11;
+        END IF;
+        SET ace_count = ace_count - 1;
+    END WHILE;
+
+    -- Set the session variable with the calculated total score
+    SET @total_score = total_score;
+END;
